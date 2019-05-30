8ba747f556ac8de5f331820ac959cef3ec68331b
MarcosFilhoBr <madlnf@hotmail.com>
2019-05-30 11:02:48 -0300

    Aula Prática 30-05-2019 -- Marcos -Commit 002 - Programação IV

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,6 +2,7 @@ package com.londonappbrewery.destini;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,7 +15,9 @@ public class MainActivity extends AppCompatActivity {
     // TODO: Declare as variaveis aqui:
 
     //indice corrente da historia
-    private Story mStorySelected;
+
+    private int mStoryIndex; // Ao adicionar condição savedInstanceState pediu para informar um TIPO.
+
 
     Story mT1 = new Story(R.string.T1_Story);
     Story mT2 = new Story(R.string.T2_Story);
@@ -28,16 +31,20 @@ public class MainActivity extends AppCompatActivity {
     Answer mA2_1 = new Answer(R.string.T2_Ans1);
     Answer mA2_2 = new Answer(R.string.T2_Ans2);
     Answer mA3_1 = new Answer(R.string.T3_Ans1);
-    Answer mA3_2 = new Answer(R.string.T2_Ans2);
-    Answer mA4   = new Answer(R.string.T4_End);
-    Answer mA5   = new Answer(R.string.T5_End);
-    Answer mA6   = new Answer(R.string.T6_End);
+    Answer mA3_2 = new Answer(R.string.T3_Ans2);
+//    Answer mA4   = new Answer(R.string.T4_End);
+//    Answer mA5   = new Answer(R.string.T5_End);
+//    Answer mA6   = new Answer(R.string.T6_End);
 
+    private Story mStorySelected;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(savedInstanceState!=null) {
+            mStoryIndex = savedInstanceState.getInt("StoryKey");
+        }
 
         //TODO: Faça o link do layout com a activity
 
@@ -50,21 +57,63 @@ public class MainActivity extends AppCompatActivity {
 
         mT1.setAnswerTop(mA1_1);
         mT1.setAnswerBottom(mA1_2);
-        mA1_1.setChildStory(mT3);
-        mA1_2.setChildStory(mT2);
+          mA1_1.setChildStory(mT3);
+          mA1_2.setChildStory(mT2);
 
         mT2.setAnswerTop(mA2_1);
         mT2.setAnswerBottom(mA2_2);
-        mA2_1.setChildStory(mT3);
-        mA2_2.setChildStory(mT5);
+          mA2_1.setChildStory(mT3);
+          mA2_2.setChildStory(mT4);
 
-        //mT3
+        mT3.setAnswerTop(mA3_1);
+        mT3.setAnswerBottom(mA3_2);
+          mA3_1.setChildStory(mT6);
+          mA3_2.setChildStory(mT5);
+
 
 
         // TODO: Coloque o evento do click do botão, caso precise colocar a visibilidade no botão invisivel utilize a função
         // do botão setVisibility(View.GONE):
+        mStorySelected = mT1;
+        mStoryTextView.setText(mStorySelected.getStoryID());
+        mAnswerTop.setText(mStorySelected.getAnswerTop().getAnswerID());
+        mAnswerBottom.setText(mStorySelected.getAnswerBottom().getAnswerID());
 
+        mAnswerTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mStorySelected = mStorySelected.getAnswerTop().getChildStory();
+                mStoryTextView.setText(mStorySelected.getStoryID());
+                if(mStorySelected.getAnswerTop() == null){
+                    mAnswerTop.setVisibility(View.GONE);
+                    mAnswerBottom.setVisibility(View.GONE);
+                }else{
+                    mAnswerTop.setText(mStorySelected.getAnswerTop().getAnswerID());
+                    mAnswerBottom.setText(mStorySelected.getAnswerBottom().getAnswerID());
+                }
+            }
+        });
 
+        mAnswerBottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mStorySelected = mStorySelected.getAnswerBottom().getChildStory();
+                mStoryTextView.setText(mStorySelected.getStoryID());
+                if(mStorySelected.getAnswerBottom() == null){
+                    mAnswerTop.setVisibility(View.GONE);
+                    mAnswerBottom.setVisibility(View.GONE);
+                }else{
+                    mAnswerTop.setText(mStorySelected.getAnswerTop().getAnswerID());
+                    mAnswerBottom.setText(mStorySelected.getAnswerBottom().getAnswerID());
+                }
+            }
+        });
+
+    }
+
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putInt("StoryKey",mStoryIndex);
     }
 
 }
